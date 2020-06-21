7cea0074c4617fdb7920febc27c035f1b843760a
David Obodo <obododavid5@gmail.com>
2020-06-21 02:40:43 +0100

    set up global styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+*, 
+  *:before, 
+  *:after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+
+  body{
+    font-size: 1.4rem;
+    font-family: 'Lato', sans-serif;
+    color: #444;
+  }
+`;
+
+const theme = {};
 
 const App = () => {
-    return <div></div>;
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <div>hello</div>;
+        </ThemeProvider>
+    );
 };
 
 export default App;
